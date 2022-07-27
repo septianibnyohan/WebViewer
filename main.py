9329019811381c49150de0205a7a61b58efc6a07
Septian Ibnu Yohan <septianibnyohan@gmail.com>
2022-07-27 08:15:33 +0700

    initial

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import json
 import os
+from random import choice
+
 import requests
 from fake_headers import Headers, browsers
 from requests.exceptions import RequestException
@@ -12,14 +15,39 @@ from webviewer.basic import type_keyword
 from webviewer.download_driver import *
 import openpyxl
 
+from webviewer.proxies import scrape_api
+
+proxy = None
+
+used_proxies = []
+
 cwd = os.getcwd()
 patched_drivers = os.path.join(cwd, 'patched_drivers')
+config_path = os.path.join(cwd, 'config.json')
+
+viewports = ['2560,1440', '1920,1080', '1440,900',
+             '1536,864', '1366,768', '1280,1024', '1024,768']
 
 def main():
-    return
+    global cancel_all, proxy_list, total_proxies, proxies_from_api, threads, hash_config, futures, cpu_usage
+
+    if category == 'r' and proxy_api:
+        proxies_from_api = scrape_api(link=filename)
 
 def main_viewer(proxy_type, proxy, position):
-    return
+    header = Headers(
+        browser="chrome",
+        os=osname,
+        headers=False
+    ).generate()
+    agent = header['User-Agent']
+
+    if category == 'r' and proxy_api:
+        for _ in range(20):
+            proxy = choice(proxies_from_api)
+            if proxy not in used_proxies:
+                break
+        used_proxies.append(proxy)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -42,6 +70,28 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     update_chrome_version()
     osname, exe_name = download_driver(patched_drivers=patched_drivers)
+
+    with open(config_path, 'r', encoding='utf-8-sig') as openfile:
+        config = json.load(openfile)
+
+    api = config["http_api"]["enabled"]
+    host = config["http_api"]["host"]
+    port = config["http_api"]["port"]
+    database = config["database"]
+    views = config["views"]
+    minimum = config["minimum"] / 100
+    maximum = config["maximum"] / 100
+    category = config["proxy"]["category"]
+    proxy_type = config["proxy"]["proxy_type"]
+    filename = config["proxy"]["filename"]
+    auth_required = config["proxy"]["authentication"]
+    proxy_api = config["proxy"]["proxy_api"]
+    refresh = config["proxy"]["refresh"]
+    background = config["background"]
+    bandwidth = config["bandwidth"]
+    playback_speed = config["playback_speed"]
+    max_threads = config["max_threads"]
+    min_threads = config["min_threads"]
 
     xlsx_file = "data.xlsx"
     wb_obj = openpyxl.load_workbook(xlsx_file)
